edae32fc06e54d5a4034adc8ed5ca1fd0dcb8b95
da.aldridge <da.aldridge@proton.me>
2024-01-26 05:19:22 -0500

    created main components; styled component widths; added hero image

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
 import './App.css'
+import Portfolio from './components/Portfolio'
+import Sidebar from './components/Sidebar'
 
 function App() {
 
   return (
-    <div>
-      <p className="text-3xl">Hello world.</p>
+    <div className="w-screen h-screen flex flex-row justify-between">
+      <div className="h-screen w-1/6 bg-zinc-900">
+        <Sidebar />
+      </div>
+      <div className="h-screen w-full bg-zinc-600">
+        <Portfolio />
+      </div>
     </div>
   )
 }
---
+++ src/components/Portfolio.tsx
@@ -0,0 +1,5 @@
+export default function Portfolio() {
+    return (
+        <p>portfolio component</p>
+    )
+}
