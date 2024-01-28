665121700d97bfd5f093c1c2e000e703a59c46f2
da.aldridge <da.aldridge@proton.me>
2024-01-28 05:37:02 -0500

    reorganized component directory; added scrollIntoView functionality to sidebar

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import './App.css'
-import About from './components/Portfolio/About'
-import Contact from './components/Portfolio/Contact'
-import Freelancing from './components/Portfolio/Freelancing'
-import Hero from './components/Portfolio/Hero'
-import Projects from './components/Portfolio/Projects'
-import Tech from './components/Portfolio/Tech'
+import About from './components/About'
+import Contact from './components/Contact'
+import Freelancing from './components/Freelancing'
+import Hero from './components/Hero'
+import Projects from './components/Projects'
+import Tech from './components/Tech'
+import { useRef } from 'react';
 import { BsTwitterX, BsWindowStack } from 'react-icons/bs'
 import { FaDiscord, FaLinkedinIn, FaListUl } from 'react-icons/fa6'
 import { GrHomeRounded, GrBriefcase } from 'react-icons/gr'
@@ -13,6 +14,12 @@ import { TfiIdBadge, TfiEmail } from 'react-icons/tfi'
 
 export default function App() {
   const smallIcon = "1rem";
+  const navHome = useRef<HTMLInputElement>(null);
+  const navAbout = useRef<HTMLInputElement>(null);
+  const navTech = useRef<HTMLInputElement>(null);
+  const navProjects = useRef<HTMLInputElement>(null);
+  const navFreelancing = useRef<HTMLInputElement>(null);
+  const navContact = useRef<HTMLInputElement>(null);
 
   return (
     <div className="w-screen h-screen flex flex-row">
@@ -44,32 +51,42 @@ export default function App() {
                   </div>
               </div>
               <div className="flex flex-col justify-between pt-16 text-xl gap-8">
-                  <div className="flex flex-row items-center gap-4 w-36">
-                      <GrHomeRounded />
-                      <p>Home</p>
-                  </div>
-                  <div className="flex flex-row items-center gap-4 w-36">
-                      <TfiIdBadge />
-                      <p>About</p>
-                  </div>
-                  <a>
-                    <div className="flex flex-row items-center gap-4 w-36">
-                        <FaListUl />
-                        <p>Skills</p>
-                    </div>
-                  </a>
-                  <div className="flex flex-row items-center gap-4 w-36">
-                      <BsWindowStack />
-                      <p>Projects</p>
-                  </div>
-                  <div className="flex flex-row items-center gap-4 w-36">
-                      <GrBriefcase />
-                      <p>Freelancing</p>
-                  </div>
-                  <div className="flex flex-row items-center gap-4 w-36">
-                      <TfiEmail />
-                      <p>Contact</p>
-                  </div>
+                    <a onClick={() => navHome.current!.scrollIntoView({behavior: 'smooth'})}>
+                        <div className="flex flex-row items-center gap-4 w-36">
+                            <GrHomeRounded />
+                            <p>Home</p>
+                        </div>
+                    </a>
+                    <a onClick={() => navAbout.current!.scrollIntoView({behavior: 'smooth'})}>
+                        <div className="flex flex-row items-center gap-4 w-36">
+                            <TfiIdBadge />
+                            <p>About</p>
+                        </div>
+                    </a>
+                    <a onClick={() => navTech.current!.scrollIntoView({behavior: 'smooth'})}>
+                        <div className="flex flex-row items-center gap-4 w-36">
+                            <FaListUl />
+                            <p>Skills</p>
+                        </div>
+                    </a>
+                    <a onClick={() => navProjects.current!.scrollIntoView({behavior: 'smooth'})}>
+                        <div className="flex flex-row items-center gap-4 w-36">
+                            <BsWindowStack />
+                            <p>Projects</p>
+                        </div>
+                    </a>
+                    <a onClick={() => navFreelancing.current!.scrollIntoView({behavior: 'smooth'})}>
+                        <div className="flex flex-row items-center gap-4 w-36">
+                            <GrBriefcase />
+                            <p>Freelancing</p>
+                        </div>
+                     </a>
+                     <a onClick={() => navContact.current!.scrollIntoView({behavior: 'smooth'})}>
+                        <div className="flex flex-row items-center gap-4 w-36">
+                            <TfiEmail />
+                            <p>Contact</p>
+                        </div>
+                    </a>
               </div>
           </div>
           <div className="w-full">
@@ -82,14 +99,24 @@ export default function App() {
       </div>
       <div className="w-full max-h-screen overflow-y-auto no-scrollbar">
         <div className="bg-hero bg-fixed bg-cover h-screen">
-            <Hero />
-            <About />
-            <section>
+            <section ref={navHome} className="h-screen">
+                <Hero />
+            </section>
+            <section ref={navAbout}>
+                <About />
+            </section>
+            <section ref={navTech}>
               <Tech />
             </section>
-            <Projects />
-            <Freelancing />
-            <Contact />
+            <section ref={navProjects}>
+                <Projects />
+            </section>
+            <section ref={navFreelancing}>
+                <Freelancing />
+            </section>
+            <section ref={navContact}>
+                <Contact />
+            </section>
         </div>
       </div>
     </div>
